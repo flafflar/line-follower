9dcad2e39a7e17428a6c4eea84ed84a2a8772c55
Achilleas Michailidis <achmichail@gmail.com>
2024-03-31 18:21:49 +0300

    test: create test program for multiplexer driver

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+#include "mux.h"
 
 int main(void) {
 	stdio_init_all();
 
+	mux_init();
+	
+	int channel = 0;
 	while (true) {
-		printf("Hello world!\n");
-		sleep_ms(1000);
+		int c = getchar_timeout_us(0);
+		if (c != PICO_ERROR_TIMEOUT) {
+			channel = (c - '0') % 8;
+		}
+		
+		uint16_t value = mux_read(channel);
+		printf("Channel %d: %04d \r", channel, value);
 	}
 }
