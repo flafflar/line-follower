bf34fdf32878b9d492fc79f18bfc5eb9879b4007
Achilleas Michailidis <achmichail@gmail.com>
2024-04-05 14:32:05 +0300

    test: create test program to display sensor values

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "mux.h"
+#include "sensor.h"
+
 int main(void) {
 	stdio_init_all();
 
+	mux_init();
+
 	while (true) {
-		printf("Hello world!\n");
+		uint16_t sensor[8];
+		sensor_read(sensor);
+
+		for (int s = 0; s < 8; s++) {
+			printf("%05d ", sensor[s]);
+		}
+		printf("\r");
+
 		sleep_ms(1000);
 	}
 }
